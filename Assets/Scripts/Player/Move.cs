02d35935e50a416aa1dd92dfd8328c43ad9337dd
AxelAl <axel.alzate.99@gmail.com>
2024-04-04 13:09:29 -0500

    daño aun no funciona

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float speed = 5.0f; // Velocidad de movimiento
+     public int health = 100;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +23,17 @@ public class Move : MonoBehaviour
         Vector2 movement = new Vector2(moveHorizontal, moveVertical); // Crea un vector de movimiento
 
         GetComponent<Rigidbody2D>().velocity = movement * speed; // Aplica el movimiento al Rigidbody2D del jugador
+        if (health <= 0) // Si la salud es 0 o menos
+        {
+            Destroy(gameObject); // Destruye el objeto del jugador
+        }
+    
+
+    
+    }
+    // Función para recibir daño
+    public void TakeDamage(int damage)
+    {
+        health -= damage; // Reduce la salud del jugador
     }
 }
